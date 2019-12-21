850fb850f31d6b866d65886bdb56166b1e06b8ed
Michael-Kamau <kamau.karitu@gmail.com>
2019-12-22 02:16:05 +0300

    added table to the homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  amount: number =0;
+
+  products:Array<object>=[
+    {product:'Toyota',price:1000},
+    {product:'Mazda',price:1500},
+    {product:'Isuzu',price:3000},
+    {product:'Nissan',price:2500},
+    {product:'Subaru',price:1600}
+
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
